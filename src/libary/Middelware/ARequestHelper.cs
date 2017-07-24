9e58e9296c40118af5aa1b3013e92325acc9601f
Dev-Owl <info@devowl.de>
2017-07-24 22:42:27 +0200

    Attachment support

--- src/libary/Middelware/ARequestHelper.cs
+++ src/libary/Middelware/ARequestHelper.cs
@@ -24,6 +24,7 @@ namespace PillowSharp.Middelware
         public abstract Task<IRestResponse> GetDocument(string ID,string Database,string Revision=null);
         
         public abstract Task<IRestResponse> Get(string Url);
+
         public abstract Task<IRestResponse> Get(string Url,params KeyValuePair<string,object>[] Parameter);
         
         public abstract Task<IRestResponse> Put(string Url,string Body=null);
@@ -31,6 +32,13 @@ namespace PillowSharp.Middelware
         public abstract Task<IRestResponse> Delete(string Uri);
 
         public abstract Task<IRestResponse> Post(string Uri,string Body = null);
+
+        public abstract Task<IRestResponse> UploadFile(string ID,string AttachmentName,string Revision,string Database,string File);
+
+        public abstract Task<IRestResponse> DeleteFile(string ID,string AttachmentName,string Revision,string Database);
+
+        public abstract Task<IRestResponse> GetFile(string ID,string AttachmentName,string Revision,string Database);
+
 
     }
 }

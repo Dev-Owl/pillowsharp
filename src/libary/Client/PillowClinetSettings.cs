f2470358f657ff780ae2ba5afe41a35b95b42337
Dev-Owl <info@devowl.de>
2017-07-29 18:06:48 +0200

    Added setting class with defaults for client

---
+++ src/libary/Client/PillowClinetSettings.cs
@@ -0,0 +1,29 @@
+using PillowSharp.Middelware;
+using PillowSharp.Middelware.Default;
+using PillowSharp.CouchType;
+using PillowSharp.BaseObject;
+
+
+using System;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.Collections.Generic;
+using RestSharp;
+using System.Linq;
+using System.IO;
+using PillowSharp.Helper;
+using System.Net;
+
+namespace PillowSharp.Client
+{
+    public class PillowClientSettings
+    {
+        public bool AutoGenerateID { get; set; } = true;
+
+        public bool UseCouchUUID { get; set; } = false;
+
+        public bool IgnoreJSONNull { get; set; } = true;
+
+        
+    }
+}

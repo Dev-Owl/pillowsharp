d8a1922c2211ee7ea8cfbd6652e93d01556ea3fb
Dev-Owl <info@devowl.de>
2017-07-29 17:31:22 +0200

    Fixed build script

--- src/build.sh
+++ src/build.sh
@@ -2,17 +2,24 @@
 #exit if any command fails
 set -e
 cd "$(dirname "$0")"
-artifactsFolder="./artifacts"
-
+#clean up what was left over
+cd libary
+artifactsFolder="./nuget"
 if [ -d $artifactsFolder ]; then  
   rm -R $artifactsFolder
 fi
-cd libary
+#restore and build lib
 dotnet restore
 dotnet build
+#go to test dir
 cd ../test
+#restore,build and run tests
+dotnet restore
+dotnet build
 dotnet test
 revision=${TRAVIS_JOB_ID:=1}  
 revision=$(printf "%04d" $revision) 
-cd ../libary
-dotnet pack  -c Release -o ./artifacts
+#back to root
+cd ..
+#create a nuget to test release building as last step
+dotnet pack ./libary/pillowsharp.csproj  -c Release -o ./nuget

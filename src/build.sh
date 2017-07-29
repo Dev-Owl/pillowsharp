b09aeb5e1890c6317cfcac977e2d9909c06605b9
Dev-Owl <info@devowl.de>
2017-07-29 17:11:18 +0200

    Build script added Restore Build debug Test Build release

---
+++ src/build.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+#exit if any command fails
+set -e
+
+artifactsFolder="./artifacts"
+
+if [ -d $artifactsFolder ]; then  
+  rm -R $artifactsFolder
+fi
+cd libary
+dotnet restore
+dotnet build
+cd ../test
+dotnet test
+revision=${TRAVIS_JOB_ID:=1}  
+revision=$(printf "%04d" $revision) 
+cd ../libary
+dotnet pack  -c Release -o ./artifacts

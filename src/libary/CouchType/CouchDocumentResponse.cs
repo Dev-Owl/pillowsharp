480f59e3d188753813b4b560177554f7c846f430
Christian <info@devowl.de>
2017-08-02 15:26:35 +0200

    Changed to nullable depending of the call couch doesnt send this values for all requests

--- src/libary/CouchType/CouchDocumentResponse.cs
+++ src/libary/CouchType/CouchDocumentResponse.cs
@@ -6,9 +6,11 @@ namespace PillowSharp.CouchType
 {
     public class CouchDocumentResponse<T> where T : new()
     {
-        public int total_rows { get; set; }
+        public int? total_rows { get; set; }
 
-        public int offset { get; set; }
+        public int? offset { get; set; }
+
+        public int? update_seq  { get; set; }
 
         public List<T> rows { get; set; }             
     }

1608c19028818efeb1c1bac16310a6782a0cc913
Dev-Owl <info@devowl.de>
2017-08-02 19:30:09 +0200

    Added class to collect shared couch entry points

---
+++ src/libary/CouchType/CouchEntryPoints.cs
@@ -0,0 +1,14 @@
+public static class CouchEntryPoints
+{
+         //List of defined entry points in couchDB
+        public const string AllDBQuery = "_all_dbs";
+        public const string BulkOperation = "_bulk_docs";
+        public const string Session = "_session";
+        public const string CoockieName = "AuthSession";
+        public const string NewUUID = "_uuids";
+        public const string ParamCount = "count";
+        public const string AllDocs="_all_docs";
+        
+        public const string DesignDoc ="_design";
+        public const string ViewDoc ="_view";
+}

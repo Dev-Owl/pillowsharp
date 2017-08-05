1aaf552bc188f736702d36ccab9e7d126f9ef8b9
Dev-Owl <info@devowl.de>
2017-08-05 21:43:53 +0200

    Helper Class to store a key and a value without any other magic

---
+++ src/libary/Helper/KeyValue.cs
@@ -0,0 +1,30 @@
+
+using System;
+using System.Net;
+using System.Threading.Tasks;
+using PillowSharp.BaseObject;
+using PillowSharp.CouchType;
+using PillowSharp.Middelware;
+using RestSharp;
+
+namespace PillowSharp.Helper
+{
+    public class KeyValue<TKey,TValue>
+    {
+
+        public KeyValue(TKey Key,TValue Value)
+        {
+            this.Key = Key;
+            this.Value = Value;
+        }
+
+        public KeyValue()
+        {
+            
+        }
+
+        public TKey Key { get; set; }
+
+        public TValue Value { get; set; }
+    }
+}

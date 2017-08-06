ab377486031c1f4147eaad00b420bd24fe56bdd8
Dev-Owl <info@devowl.de>
2017-08-06 16:30:38 +0200

    Added option to create client with DB in tests directly

--- src/test/CouchTestSettings.cs
+++ src/test/CouchTestSettings.cs
@@ -7,7 +7,7 @@ public static class CouchSettings{
 
     public static BasicCouchDBServer Server = new BasicCouchDBServer("http://127.0.0.1:5984",new CouchLoginData("admin","admin"),ELoginTypes.BasicAuth);
 
-    public static PillowClient GetTestClient() {
-        return new PillowClient(CouchSettings.Server); 
+    public static PillowClient GetTestClient(string Database=null) {
+        return new PillowClient(CouchSettings.Server) {Database = Database}; 
     }
 }

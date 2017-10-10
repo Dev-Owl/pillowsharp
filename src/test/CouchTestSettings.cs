4ca7e31a59e4999bebdb0f4d2b9f0c87996a703a
DevOwl <info@devowl.de>
2017-10-10 23:39:14 +0200

    Fixed name

--- src/test/CouchTestSettings.cs
+++ src/test/CouchTestSettings.cs
@@ -7,9 +7,9 @@ public static class CouchSettings{
 
     public static bool SkipAuthTests { get; set; } = false;
 
-    public static BasicCouchDBServer Server = new BasicCouchDBServer("http://127.0.0.1:5984",new CouchLoginData("admin","admin"),ELoginTypes.Token);
+    public static BasicCouchDBServer Server = new BasicCouchDBServer("http://127.0.0.1:5984",new CouchLoginData("admin","admin"),ELoginTypes.TokenLogin);
 
-    public static PillowClient GetTestClient(string Database=null, ELoginTypes LoginType = ELoginTypes.Token) {
+    public static PillowClient GetTestClient(string Database=null, ELoginTypes LoginType = ELoginTypes.TokenLogin) {
         Server.LoginType = LoginType;
         return new PillowClient(CouchSettings.Server) {Database = Database}; 
     }

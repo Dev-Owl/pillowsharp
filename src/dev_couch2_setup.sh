0c28b8f9ecdd868c9bc90b69b2367dea5fb4ec20
DevOwl <info@devowl.de>
2017-10-14 16:58:49 +0200

    Setup script couch2 added

---
+++ src/dev_couch2_setup.sh
@@ -0,0 +1,1 @@
+curl -H "Content-Type: application/json" -X POST -d '{"action":"enable_single_node","username":"admin","password":"admin","bind_address":"0.0.0.0","port":5984,"singlenode":true}' http://localhost:5984/_cluster_setup

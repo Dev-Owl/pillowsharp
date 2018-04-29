b101af12a192262d759ce79cd9c907caf056e4d1
DevOwl <info@devowl.de>
2018-04-29 13:29:39 +0200

    Travis fixes

--- src/dev_couch2_setup.sh
+++ src/dev_couch2_setup.sh
@@ -1,1 +1,2 @@
+#!/usr/bin/env bash
 curl -H "Content-Type: application/json" -X POST -d '{"action":"enable_single_node","username":"admin","password":"admin","bind_address":"0.0.0.0","port":5984,"singlenode":true}' http://localhost:5984/_cluster_setup

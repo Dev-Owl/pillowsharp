0999309f165e1dc65db1ed8173f55ed34f9f1fb3
Dev-Owl <info@devowl.de>
2018-04-29 13:43:18 +0200

    travis change number 603456789

--- src/dev_couch2_setup.sh
+++ src/dev_couch2_setup.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 curl http://localhost:5984/
-curl -H "Content-Type: application/json" -X POST -d '{"action":"enable_single_node","username":"admin","password":"admin","bind_address":"0.0.0.0","port":5984,"singlenode":true}' http://localhost:5984/_cluster_setup
+curl -X PUT localhost:5984/_config/admins/admin -d '"admin"'

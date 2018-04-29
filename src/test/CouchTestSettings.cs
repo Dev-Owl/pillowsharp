af36664e2485873b609f6c647d4ef915fd129d0b
Dev-Owl <info@devowl.de>
2018-04-29 15:59:46 +0200

    Changed default login types for tests

--- src/test/CouchTestSettings.cs
+++ src/test/CouchTestSettings.cs
@@ -9,7 +9,7 @@ public static class CouchSettings{
 
     public static BasicCouchDBServer Server = new BasicCouchDBServer("http://127.0.0.1:5984",new CouchLoginData("admin","admin"),ELoginTypes.TokenLogin);
 
-    public static PillowClient GetTestClient(string Database=null, ELoginTypes LoginType = ELoginTypes.TokenLogin) {
+    public static PillowClient GetTestClient(string Database=null, ELoginTypes LoginType = ELoginTypes.BasicAuth) {
         Server.LoginType = LoginType;
         return new PillowClient(CouchSettings.Server) {ForcedDatabaseName = Database}; 
     }

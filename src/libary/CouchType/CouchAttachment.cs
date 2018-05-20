7d430bd78dfa3406aa1154a9d1ebfca9c1dc5d57
Christian <info@devowl.de>
2018-05-20 11:52:39 +0200

    Added Base attachement model for couchdb

---
+++ src/libary/CouchType/CouchAttachment.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PillowSharp.CouchType
+{
+    public class CouchAttachment
+    {
+        [JsonProperty("content_type")]
+        public string ContentType { get; set; }
+
+        [JsonProperty("revpos")]
+        public string Revpos { get; set; }
+
+        [JsonProperty("digest")]
+        public string Digest { get; set; }
+
+        [JsonProperty("length")]
+        public Int64 Length { get; set; }
+
+        [JsonProperty("stub")]
+        public bool Stub { get; set; }
+    }
+}
